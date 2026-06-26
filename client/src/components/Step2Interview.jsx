83a96204b7134da4f230c3b34e1de0cd41aa1ecf
priyanshi012 <priyanshisaini1703@gmail.com>
2026-06-27 00:57:14 +0530

    create outer view

--- client/src/components/Step2Interview.jsx
+++ client/src/components/Step2Interview.jsx
@@ -1,8 +1,23 @@
 import React from 'react'
 
+
+
+
+
 function Step2Interview({interviewData, onFinish}) {
+   const {interviewId , questions, userName} = interviewData 
+
   return (
-    <div>
+    <div className=' min-h-screen bg-gradient-to-br from-emerald-50 via-white to-teal-100 flex items-center justify-center p-4 sm:p-6'>
+      <div className='w-full max-w-350 min-h-[80vh] bg-white rounded-3xl shadow-2xl border border-gray-200 flex flex-col lg:flex-row overflow-hidden'>
+
+  {/* video */}
+  <div className='w-full lg:w-[35%] bg-white flex flex-col items-center p-6 space-y-6 border-r border-gray-200'>
+
+  </div>
+
+
+      </div>
       
     </div>
   )
