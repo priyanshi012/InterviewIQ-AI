e549a16b2e9c7ed5651833dfefcb1f5f58c2e860
priyanshi012 <priyanshisaini1703@gmail.com>
2026-07-08 19:42:35 +0530

    complete interview setup

--- server/controllers/interview.controller.js
+++ server/controllers/interview.controller.js
@@ -383,3 +383,61 @@ return res.status(200).json({
     return res.status(500).json({message:`failed to finish interview ${error}`})
   }
 }
+
+
+export const getMyInterviews = async(req,res)=>{
+ try {
+  const interview = await Interview.findOne({userId:req.userId})
+  .sort({creditedAt:-1})
+  .select("role experience mode finalScore status creditedAt")
+
+  return res.status(200).json(interview)
+ } catch (error) {
+    return res.status(500).json({message:`failed to find current Interview ${error}`})
+ }
+}
+
+export const getInterviewReport = async(req,res)=>{
+  try {
+    const interview = await Interview.findById(req.params.id)
+
+    if(!interview){
+      return res.status(404).json({message:`Interview not found`})
+    }
+
+    const totalQuestions = interview.questions.length;
+
+  
+  let totalConfidence =0;
+  let totalCommunication =0;
+  let totalCorrectness =0;
+
+  interview.questions.forEach((q) => {
+   
+    totalConfidence += q.confidence||0;
+    totalCommunication += q.communication||0;
+    totalCorrectness += q.correctness ||0;
+    
+
+  });
+ 
+
+  const avgConfidence = totalQuestions?totalConfidence/totalQuestions :0;
+
+  const avgCommunication = totalQuestions?totalCommunication/totalQuestions :0;
+
+  const avgCorrectness = totalQuestions?totalCorrectness/totalQuestions :0;
+
+return res.json({
+    finalScore: interview.finalScore,
+    confidence: Number(avgConfidence.toFixed(1)),
+    communication: Number(avgCommunication.toFixed(1)),
+    correctness: Number(avgCorrectness.toFixed(1)),
+    questionWiseScore: interview.questions
+  });
+
+  } catch (error) {
+    return res.status(500).json({message:`failed to find currectUser Interview ${error}`})
+    
+  }
+}
