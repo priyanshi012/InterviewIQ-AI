a05674b888524530a180c6a245c6b585cbfdedb4
Priyanshi <priyanshisaini1703@gmail.com>
2026-07-28 12:25:20 +0530

    Update index.js

--- server/index.js
+++ server/index.js
@@ -10,7 +10,7 @@ dotenv.config()
 
 const app = express()
 app.use(cors({
-  origin:"http://localhost:5173",
+  origin:"https://interviewiq-ai-1-roeq.onrender.com",
   credentials:true
 }))
 //change
